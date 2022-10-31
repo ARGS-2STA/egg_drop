bec109cec9b67d19db51d452e1f64628eacd8638
Tien <tient2477@gmail.com>
2022-10-31 15:44:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def main() -> None:
+    tyngdeakselerasjon = 9.81
+    høyde = 2.5
+    luftmotstand_koeffisient = 0.07
+    masse = 60 / 1000
+
+    startfart = 0
+
+    delta_tid = 0.1
+
+    def a(v: float) -> float:
+        # sum_f = m*a
+        # a = sum_f/m
+        # sum_f = G - L
+        G = tyngdeakselerasjon * masse
+        L = luftmotstand_koeffisient * v**2
+
+        return (G - L) / masse
+
+
+if __name__ == "__main__":
+    main()
